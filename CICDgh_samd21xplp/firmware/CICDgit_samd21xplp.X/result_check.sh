882ffdfdb42ece962b1861cc26558d52734eccb6
Stefan Luethin <Stefan.Luethin@microchip.com>
2022-05-13 15:41:24 +0200

    changed path where find should search to <.> instead of <./dist>

--- CICDgh_samd21xplp/firmware/CICDgit_samd21xplp.X/result_check.sh
+++ CICDgh_samd21xplp/firmware/CICDgit_samd21xplp.X/result_check.sh
@@ -2,7 +2,8 @@
 #- Description: simple find to get path+name of elf-file
 #- v1.0: first version (SL, 10.5.22)
 
-resultf_n=`find ./dist -name *.elf`
+#resultf_n=`find ./dist -name *.elf`
+resultf_n=`find . -name *.elf`
 #echo "result check - start"
 if [ "${resultf_n}" != "" ]; then
     echo "Resultfile = -${resultf_n}-"
