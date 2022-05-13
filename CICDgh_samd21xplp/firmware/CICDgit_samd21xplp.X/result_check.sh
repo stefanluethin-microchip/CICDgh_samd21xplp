6d3809a37700530ef6c8f896cc2076f90d3338d9
Stefan Luethin <Stefan.Luethin@microchip.com>
2022-05-13 15:05:47 +0200

    fixed script to use find to get path and name of final *.elf

--- CICDgh_samd21xplp/firmware/CICDgit_samd21xplp.X/result_check.sh
+++ CICDgh_samd21xplp/firmware/CICDgit_samd21xplp.X/result_check.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+#- Description: simple find to get path+name of elf-file
+#- v1.0: first version (SL, 10.5.22)
 
-#resultf_p=./dist/samd21xplp/production/*.elf
 resultf_n=`find ./dist -name *.elf`
-
-if [[ "${resultf_n}" != "" ]]; then
-    echo "Resultfile ${resultf_n}"
+#echo "result check - start"
+if [ "${resultf_n}" != "" ]; then
+    echo "Resultfile = -${resultf_n}-"
 else
     echo "no resultfile"
 fi
+#echo "result check - done"
